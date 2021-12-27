10adb51ab00c8422da04bfe1891efde3d2c60119
enochviewu95 <viewuenoch@gmail.com>
2021-12-27 09:27:26 +0000

    Update app.js

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -13,9 +13,9 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = '6f9c651f02e44fc48b0a0cf6a2aee799'; // Your client id
+var client_secret = 'bf67e0ad4c2a4af4aed5cc60e5799848'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
